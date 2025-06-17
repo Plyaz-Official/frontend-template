4d6681423bc67c870500dbf9c21a78ee436ab9f2
Red <red.pace.dev@gmail.com>
2025-06-17 22:47:35 +0200

    feat: Adjusted frontend template to support `@plyaz/devtools`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Plyaz Fe Template",
-  description: "Plyaz Frontend Template",
+  title: 'Plyaz Fe Template',
+  description: 'Plyaz Frontend Template',
 };
 
 export default function RootLayout({
