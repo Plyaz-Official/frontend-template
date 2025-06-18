e4d54e8edad6191272e3a992d60e6d565db8d176
Red <red.pace.dev@gmail.com>
2025-06-18 22:20:28 +0200

    feat: Updated tailwind.config to use `@plyaz/devtools`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,3 @@
-import type { Config } from 'tailwindcss';
+import baseConfig from '@plyaz/devtools/configs/tailwind.config';
 
-export default {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+export default baseConfig;
