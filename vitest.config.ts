d3235390613403a357452a7887af1075f00072fa
Red <red.pace.dev@gmail.com>
2025-06-19 21:57:52 +0200

    chore: Updated dir for tests

--- vitest.config.ts
+++
@@ -1,3 +0,0 @@
-import plyazConfig from '@plyaz/devtools/configs/vitest.config';
-
-export default plyazConfig;
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,1 @@
-import '@plyaz/devtools/configs/vitest.setup';
+import '@plyaz/devtools/configs/vitest.setup.mjs';
