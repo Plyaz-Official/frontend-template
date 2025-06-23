f1d7e641760c18aabcbf12af8f2ade2ee211c75a
Red <red.pace.dev@gmail.com>
2025-06-24 00:20:46 +0200

    fix: Fix formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,27 @@ import React from 'react';
 
 export default function Home(): React.ReactElement {
   return (
-    <div className={`
+    <div
+      className={`
       grid min-h-screen grid-rows-[20px_1fr_20px] items-center
       justify-items-center gap-16 p-8 pb-20
       font-[family-name:var(--font-geist-sans)]
       sm:p-20
-    `}>
-      <main className={`
+    `}
+    >
+      <main
+        className={`
         row-start-2 flex flex-col items-center gap-[32px]
         sm:items-start
-      `}>
+      `}
+      >
         Home
       </main>
-      <footer className={`
+      <footer
+        className={`
         row-start-3 flex flex-wrap items-center justify-center gap-[24px]
-      `}>
+      `}
+      >
         <a
           href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
           rel='noopener noreferrer'
